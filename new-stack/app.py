0b25023b935fe8e3c081eaaf09206e74652d75f1
Robson Alves <robson.infoo@gmail.com>
2024-06-24 16:15:25 -0300

    including a new stack and reference between multiple stacks, moving vpc creation to a new one stack and using it at the complete stack to run nginx

--- new-stack/app.py
+++ new-stack/app.py
@@ -4,25 +4,14 @@ import os
 import aws_cdk as cdk
 
 from new_stack.new_stack_stack import NewStackStack
+from new_stack.network_stack import NetworkStack
 
 
 app = cdk.App()
-NewStackStack(app, "NewStackStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+network_stack = NetworkStack(app, 'NetworkStack')
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+NewStackStack(app, "NewStackStack", my_vpc=network_stack.vpc)
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
 
 app.synth()
