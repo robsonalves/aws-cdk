4b437bcc7b342567a8f58a15e925b3691270564c
Robson Alves <robson.infoo@gmail.com>
2024-06-24 12:05:40 -0300

    New mysql RDS, subnet without NAT and connection permissions to EC2

--- new-stack/new_stack/new_stack_stack.py
+++ new-stack/new_stack/new_stack_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     CfnOutput,
+    RemovalPolicy,
     Stack,
-    aws_ec2 as ec2
+    aws_ec2 as ec2,
+    aws_rds as rds
 )
 from constructs import Construct
 
@@ -14,7 +16,7 @@ class NewStackStack(Stack):
         my_vpc = ec2.Vpc(self, 'MyVpc', nat_gateways=0)
         web_server = ec2.Instance(self, 'WebServer',
                                   machine_image=ec2.MachineImage.latest_amazon_linux2(),
-                                  instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T2,
+                                  instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T3,
                                                                     instance_size=ec2.InstanceSize.MICRO),
                                   vpc=my_vpc,
                                   vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
@@ -29,6 +31,28 @@ class NewStackStack(Stack):
                                  'service nginx start')
 
         web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(80), 'Allow HTTP access from the Internet')
+        web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(22), 'Allow SSH access from the Internet')
 
         CfnOutput(self, 'WebServerDnsName',
                   value=web_server.instance_public_dns_name)
+        
+
+        #DB instance configuration
+        db_instance = rds.DatabaseInstance(self, 'DBInstance',
+                                           engine=rds.DatabaseInstanceEngine.MARIADB,
+                                           vpc=my_vpc,
+                                           vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
+                                           instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T3,
+                                                                             instance_size=ec2.InstanceSize.MICRO),
+                                            removal_policy=RemovalPolicy.DESTROY)
+
+        # Allowing connections to the DB instance
+        db_instance.connections.allow_default_port_from(web_server, 'Allow Mysql access from the web server')
+
+
+        # Installing MySQL client on the web server
+        web_server.add_user_data('yum install mysql -y')
+
+
+        CfnOutput(self, 'DbEndpoint',
+                  value=db_instance.db_instance_endpoint_address)
