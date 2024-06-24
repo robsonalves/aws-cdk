3f9e075f994ad8aeec88c52aaadc10f138f5d38f
Robson Alves <robson.infoo@gmail.com>
2024-06-24 15:59:49 -0300

    including s3 assets to change Nginx Index html and images to s3

--- new-stack/new_stack/new_stack_stack.py
+++ new-stack/new_stack/new_stack_stack.py
@@ -3,7 +3,7 @@ from aws_cdk import (
     RemovalPolicy,
     Stack,
     aws_ec2 as ec2,
-    aws_rds as rds
+    aws_s3_assets as s3
 )
 from constructs import Construct
 
@@ -28,31 +28,24 @@ class NewStackStack(Stack):
 
         web_server.add_user_data('yum updaete -y',
                                  'amazon-linux-extras install nginx1',
-                                 'service nginx start')
+                                 'rm -rf /usr/share/nginx/html/*')
 
         web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(80), 'Allow HTTP access from the Internet')
         web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(22), 'Allow SSH access from the Internet')
 
         CfnOutput(self, 'WebServerDnsName',
                   value=web_server.instance_public_dns_name)
-        
-
-        #DB instance configuration
-        db_instance = rds.DatabaseInstance(self, 'DBInstance',
-                                           engine=rds.DatabaseInstanceEngine.MARIADB,
-                                           vpc=my_vpc,
-                                           vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
-                                           instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T3,
-                                                                             instance_size=ec2.InstanceSize.MICRO),
-                                            removal_policy=RemovalPolicy.DESTROY)
-
-        # Allowing connections to the DB instance
-        db_instance.connections.allow_default_port_from(web_server, 'Allow Mysql access from the web server')
 
 
-        # Installing MySQL client on the web server
-        web_server.add_user_data('yum install mysql -y')
+        # Deploying a web page to the web server
 
+        web_page_asset = s3.Asset(self, 'WebPageAsset',
+                                  path='web_pages/index.html')
+        web_server.user_data.add_s3_download_command(bucket=web_page_asset.bucket,
+                                                     bucket_key=web_page_asset.s3_object_key,
+                                                     local_file='/usr/share/nginx/html/index.html')
+        web_page_asset.grant_read(web_server.role)
+        web_server.add_user_data('service nginx start')
 
-        CfnOutput(self, 'DbEndpoint',
-                  value=db_instance.db_instance_endpoint_address)
+        web_page_asset = s3.Asset(self, 'WebImageAsset',
+                            path='web_pages/image.png')
