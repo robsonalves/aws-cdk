4ff2a5e17b02ba7b9fd412a49998ce92deb557d5
Robson Alves <robson.infoo@gmail.com>
2024-06-25 08:06:17 -0300

    including cdk Aspects init, first impression

--- new-stack/app.py
+++ new-stack/app.py
@@ -5,6 +5,7 @@ import aws_cdk as cdk
 
 from new_stack.new_stack_stack import NewStackStack
 from new_stack.network_stack import NetworkStack
+from new_stack.aspects import MyFirstAspect
 
 
 app = cdk.App()
@@ -14,6 +15,10 @@ root_stack = cdk.Stack(app, 'RootStack')
 network_stack = NetworkStack(root_stack, 'NetworkStack')
 
 application_stack = NewStackStack(root_stack, "NewStackStack", my_vpc=network_stack.vpc)
+
+# Aspects
+
+cdk.Aspects.of(root_stack).add(MyFirstAspect())
 
 
 # Stack-Level tagging
